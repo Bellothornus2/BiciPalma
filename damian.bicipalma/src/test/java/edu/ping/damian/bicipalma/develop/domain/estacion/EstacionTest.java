1c1853f9230e03c5ccb597b42ecd8ca3df034372
bellothornus2 <divanov@cifpfbmoll.eu>
2021-02-05 15:47:01 +0100

    refactor: test

--- damian.bicipalma/src/test/java/edu/ping/damian/bicipalma/develop/domain/estacion/EstacionTest.java
+++ damian.bicipalma/src/test/java/edu/ping/damian/bicipalma/develop/domain/estacion/EstacionTest.java
@@ -1,5 +1,9 @@
 package edu.ping.damian.bicipalma.develop.domain.estacion;
 
+import java.io.ByteArrayOutputStream; // recoge el system out print
+import java.io.PrintStream; //lo transforma en algo legible
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,17 +14,35 @@ public class EstacionTest {
 	private String direccion;
     private int numAnclajes;
     private Estacion estacion;
-
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    
     @Before
     public void setupEstacion(){
+        System.setOut(new PrintStream(outContent));
         this.id = 1;
         this.direccion = "direccion de prueba";
         this.numAnclajes = 5;
         this.estacion = new Estacion(this.id, this.direccion, this.numAnclajes);
     }
+    
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     @Test
-    public void getIdTest(){
-        Assert.assertEquals(1, this.estacion.getId());
+    public void toStringTest(){
+        Assert.assertEquals(
+        "id: " + 1 + '\n' +
+        "direccion: " + "direccion de prueba" + '\n' +
+        "numeroAnclajes: " + 5,
+        this.estacion.toString());
+    }
+
+    @Test
+    public void consultarEstacionTest(){
+        System.out.print(this.estacion);
+        Assert.assertEquals(this.outContent.toString(), this.estacion);
     }
 }
