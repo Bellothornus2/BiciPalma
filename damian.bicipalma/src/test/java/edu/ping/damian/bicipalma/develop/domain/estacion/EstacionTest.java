53d035f75c5a2908c88642c6c2ac04424aae7e88
unknown <ozaaj@cifpfbmoll.eu>
2021-02-08 01:25:17 +0100

    We added the rest of the cases test

--- damian.bicipalma/src/test/java/edu/ping/damian/bicipalma/develop/domain/estacion/EstacionTest.java
+++ damian.bicipalma/src/test/java/edu/ping/damian/bicipalma/develop/domain/estacion/EstacionTest.java
@@ -7,6 +7,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.ping.damian.bicipalma.develop.domain.tarjetausuario.TarjetaUsuario;
+
 public class EstacionTest {
 
     private int id;
@@ -55,6 +57,42 @@ public class EstacionTest {
 
     @Test
     public void anclajesLibresTest(){
-        Assert.assertEquals(0,estacion.anclajesLibres());
+        Assert.assertEquals(0,this.estacion.anclajesLibres());
     }
+
+    @Test
+    public void anclarBicicletaTest(){
+        Autenticacion tarjetaUsuario = new TarjetaUsuario("hoasdasdasd", true);
+        Movil bicicleta = new Bicicleta(456);
+        this.estacion.retirarBicicleta(tarjetaUsuario);
+        Assert.assertEquals(1, this.estacion.anclajesLibres());
+        this.estacion.anclarBicicleta(bicicleta);
+        Assert.assertEquals(0, this.estacion.anclajesLibres());
+    }
+
+
+    @Test
+    public void leerTarjetaUsuarioTest(){
+        Autenticacion tarjetaUsuario = new TarjetaUsuario("Walid", true);
+        Assert.assertEquals(true,
+        this.estacion.leerTarjetaUsuario(tarjetaUsuario));
+    }
+
+    @Test
+    public void retirarBicicleta(){
+        Autenticacion tarjetaUsuario = new TarjetaUsuario("hoasdasdasd", true);
+        this.estacion.retirarBicicleta(tarjetaUsuario);
+        Assert.assertEquals(1, this.estacion.anclajesLibres());
+    }
+
+    // @Test
+    // public void consultarAnclajesTest(){
+    //     Bicicleta bicicleta = new Bicicleta(3);
+    //     Assert.AssertEquals("Anclaje " + 0 + " " + bicicleta.get,
+    //     this.estacion.toString());
+
+    //     Assert.assertEquals("Anclaje " + 0 + " " + " libre",
+    //     this.estacion.toString());
+    // }
+
 }
